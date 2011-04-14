8015510f9bbf1c03315e30934cbcf6f8817934f1
Tom Rudick <tmrudick@gmail.com>
2011-04-13 20:34:34 -0700

    Adding caption textbox handlers

--- WP7TumblrPhotoUploader/MainPage.xaml.cs
+++ WP7TumblrPhotoUploader/MainPage.xaml.cs
@@ -15,10 +15,34 @@ namespace WP7TumblrPhotoUploader
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        // Holds if the caption text box holds the default "Enter Caption" test or not
+        private bool hasDefaultText = true;
+        
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void captionTextbox_ManipulationStarted(object sender, ManipulationStartedEventArgs e)
+        {
+            if (this.hasDefaultText)
+            {
+                this.hasDefaultText = false;
+                this.captionTextbox.Text = string.Empty;
+                this.captionTextbox.TextAlignment = TextAlignment.Left;
+            }
+        }
+
+        private void captionTextbox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (this.captionTextbox.Text.Equals(string.Empty))
+            {
+                this.hasDefaultText = true;
+                this.captionTextbox.Text = "Enter a Caption...";
+                this.captionTextbox.TextAlignment = TextAlignment.Center;
+            }
+
         }
     }
 }
