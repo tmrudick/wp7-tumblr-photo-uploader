a836b436a0e68143706a3a3c3af33141bf6a94bb
Tom Rudick <tmrudick@gmail.com>
2011-04-13 22:53:27 -0700

    Adding credentials data class

---
+++ WP7TumblrPhotoUploader/TumblrCredentials.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace WP7TumblrPhotoUploader
+{
+    public class TumblrCredentials
+    {
+        public enum CredentialsType {BasicAuth, OAuth};
+
+        public CredentialsType Type
+        {
+            get
+            {
+                if (oauthToken.Equals(string.Empty))
+                {
+                    return CredentialsType.BasicAuth;
+                }
+                else
+                {
+                    return CredentialsType.OAuth;
+                }
+            }
+        }
+
+        // Old style username and password
+        private string Username { get; set; }
+        private string password { get; set; }
+
+        // New style OAuth this and that
+        private string oauthToken { get; set; }
+        private string oauthTokenSecret { get; set; }
+    }
+}
