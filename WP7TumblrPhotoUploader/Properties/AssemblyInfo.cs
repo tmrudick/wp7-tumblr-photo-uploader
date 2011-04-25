9e0111d7d3ed69b88cf9aaf2fc52f0c4623572d9
Tom Rudick <tmrudick@gmail.com>
2011-04-24 21:31:07 -0700

    Changing application name to Tumblr Poster

--- WP7TumblrPhotoUploader/Properties/AssemblyInfo.cs
+++ WP7TumblrPhotoUploader/Properties/AssemblyInfo.cs
@@ -5,12 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WP7TumblrPhotoUploader")]
+[assembly: AssemblyTitle("Tumblr Poster")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("WP7TumblrPhotoUploader")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyProduct("Tumblr Poster")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
